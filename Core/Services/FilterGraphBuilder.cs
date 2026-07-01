ed14a2727ad6dc3e16567399151ed01c9a97caff
BISCUITIC <tort24457@gamil.com>
2026-07-01 17:45:13 +0300

    Rename constants

--- Core/Services/FilterGraphBuilder.cs
+++ Core/Services/FilterGraphBuilder.cs
@@ -4,8 +4,8 @@ namespace Core.Services;
 
 public class FilterGraphBuilder
 {
-    private readonly string _outputVideoStream = "v_out";
-    private readonly string _outputAudioStream = "a_out";
+    private const string VideoStream = "v_out";
+    private const string AudioStream = "a_out";
 
     private readonly StringBuilder _builder;
     private int _index;
@@ -21,22 +21,22 @@ public class FilterGraphBuilder
         _builder.Clear();
 
         _builder.Append("-filter_complex \"");
-        _builder.Append($"[0:v]null[{_outputVideoStream}{_index}];");
-        _builder.Append($"[0:a]anull[{_outputAudioStream}];");
+        _builder.Append($"[0:v]null[{VideoStream}{_index}];");
+        _builder.Append($"[0:a]anull[{AudioStream}];");
     }
 
     public void Add(string filter)
     {
-        _builder.Append($"[{_outputVideoStream}{_index++}]");
+        _builder.Append($"[{VideoStream}{_index++}]");
         _builder.Append(filter);
-        _builder.Append($"[{_outputVideoStream}{_index}];");
+        _builder.Append($"[{VideoStream}{_index}];");
     }
 
     public string Build()
     {
         _builder.Append("\"");
-        _builder.Append($" -map \"[{_outputVideoStream}{_index}]\"");
-        _builder.Append($" -map \"[{_outputAudioStream}]\"");
+        _builder.Append($" -map \"[{VideoStream}{_index}]\"");
+        _builder.Append($" -map \"[{AudioStream}]\"");
 
         return _builder.ToString();
     }
